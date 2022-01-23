6be7190eb1205dae2b9e6aab01996fd878450c3d
Morgane Bentzinger <mo.bentzinger@gmail.com>
2022-01-23 17:42:44 -0500

    Initialize Class & examples for testing

--- src/CountIslands.java
+++ src/CountIslands.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class CountIslands {
+public class CountIslands {
+    private int[][] grid = {{1, 1, 1, 1, 0},
+                            {1, 1, 0, 1, 0},
+                            {1, 1, 0, 0, 0},
+                            {0, 0, 0, 0, 0}};
+
+    private int[][] grid2 = {{1, 1, 0, 0, 0},
+                             {1, 1, 0, 0, 0},
+                             {0, 0, 1, 0, 0},
+                             {0, 0, 0, 1, 1}};
+
+    public static void main(String[] args) {
+
+    }
 }
